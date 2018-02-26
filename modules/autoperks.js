9680b682b467d63178576ef7a633769c403fd552
ecarlson <ecarlson@liquidweb.com>
2018-02-26 14:35:23 -0500

    fluffy perks

--- modules/autoperks.js
+++ modules/autoperks.js
@@ -119,13 +119,13 @@ ratios1.appendChild(ratioPresetLabel);
 ratios1.appendChild(ratioPreset);
 //
 customRatios.appendChild(ratios2);
-//Line3
+/*Line3
 var ratios3 = document.createElement("DIV");
 ratios3.setAttribute('style', 'display: inline-block; text-align: left; width: 100%');
 var listratios3 = ["Capable","Cunning","Curious"];
 for (var i in listratios3)
     AutoPerks.createInput(listratios3[i],ratios3);
-customRatios.appendChild(ratios3);
+customRatios.appendChild(ratios3);*/
 document.getElementById("portalWrapper").appendChild(customRatios);
 
 //BEGIN AUTOPERKS SCRIPT CODE:>>>>>>>>>>>>>>
